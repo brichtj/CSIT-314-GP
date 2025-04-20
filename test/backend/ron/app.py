22b8ec2f4251e0bd5800d34f2ba1e6256881ec65
zaaoak <zaaoak@gmail.com>
2025-04-21 00:28:46 +0800

    add boilerplate for backend

---
+++ test/backend/ron/app.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Depends, Request
+from database import get_db_connection
+from controllers.auth_controller import AuthController
+from schemas.auth_schema import LoginRequest
+
+app = FastAPI()
+
+def get_conn():
+    conn = get_db_connection()
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+@app.post("/login")
+def login_route(request: LoginRequest, conn = Depends(get_conn)):
+    controller = AuthController(conn)
+    return controller.login(request)
